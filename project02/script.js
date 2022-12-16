2d0712f5b0749c6220c65cc9c91b0f1d6bcf0ce0
kawuli_abudureyimu <kawuli_abudureyimu@dsd.dokidoki-gw.com>
2022-12-16 16:00:38 +0900

    projects

---
+++ project02/script.js
@@ -0,0 +1,38 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+
+const imageCcontainerEl =document.querySelector(".image-container");
+
+const imagsEl = document.querySelectorAll("img");
+
+
+let currentImg = 1
+let timeout;
+
+nextEl.addEventListener("click", () => {
+   currentImg++
+   clearTimeout(timeout);
+   updateImg()
+})
+
+prevEl.addEventListener("click", () => {
+      currentImg--
+      clearTimeout(timeout);
+      updateImg()
+   })
+
+updateImg()
+
+function updateImg(){
+      if (currentImg > imagsEl.length) {
+            currentImg = 1;
+      } else if(currentImg < 1){
+            currentImg = imagsEl.length;
+      }
+     imageCcontainerEl.style.transform =`translateX(-${(currentImg - 1) * 500}px)`;
+     timeout = setTimeout(() =>{
+        currentImg++
+        updateImg()
+     }, 3000)
+}
+
